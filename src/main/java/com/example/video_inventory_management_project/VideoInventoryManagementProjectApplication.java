e9702afc79737d4136805e629451d9ab3d9d85fe
akif <sustgeneric@gmail.com>
2024-12-13 20:59:29 +0600

    initial setup

---
+++ src/main/java/com/example/video_inventory_management_project/VideoInventoryManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.video_inventory_management_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoInventoryManagementProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoInventoryManagementProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/video_inventory_management_project/VideoInventoryManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.video_inventory_management_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoInventoryManagementProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
